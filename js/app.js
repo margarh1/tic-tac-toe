07d86e8328bf1da1efb9b2c6495ca81de9a38fe2
margarh1 <ha.margaret1@gmail.com>
2016-10-28 14:06:50 -0700

    sanity checks for css and js work

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-// wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+  console.log('Sanity check.');
 
+
+
+  
 });
+
