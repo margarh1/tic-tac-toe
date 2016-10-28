b388327a6601fc13ad53fed6ef27bbcb21cb5469
margarh1 <ha.margaret1@gmail.com>
2016-10-28 14:39:18 -0700

    created method to reset board

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
 
   $('#board').on('.box', 'click', function() {
     console.log('Works!');
+    console.log();
   });
 
 
 });
 
 function newBoard() {
-  
+  $('.box').empty();
 }
